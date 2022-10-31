dbe4dcc3028c9382592ed3b0bf0377f2a292abf4
ndjndj <teq.ndj@gmail.com>
2022-10-31 23:26:43 +0900

    custom theme.dart

---
+++ squideck/lib/constants/custom_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  CustomTheme._();
+
+  static final instance = CustomTheme._();
+
+  Color appBar = const Color(0xFF465D4C);
+  Color letter = const Color(0xFFFAFAFA);
+}
