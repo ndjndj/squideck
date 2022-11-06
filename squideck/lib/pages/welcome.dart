8fb46758462d60e10677a7951ec7867156b57126
ndjndj <teq.ndj@gmail.com>
2022-11-06 14:57:09 +0900

    f

---
+++ squideck/lib/pages/welcome.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text('Blog')
+      ),
+    );
+  }
+}
