fd35ad41be0e0c6faaa086ddee28ce58839bd02c
ndjndj <teq.ndj@gmail.com>
2022-11-06 14:51:06 +0900

    f e

---
+++ squideck/lib/pages/common_frame.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class CommonFrame extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: ,
+    );
+  }
+}
