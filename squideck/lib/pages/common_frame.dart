b89985a86e85c2243c00764320cb026b48a60a18
ndjndj <teq.ndj@gmail.com>
2022-11-06 14:52:16 +0900

    f

--- squideck/lib/pages/common_frame.dart
+++ squideck/lib/pages/common_frame.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:squideck/constants/custom_theme.dart';
 
 class CommonFrame extends StatelessWidget {
-
+  
 
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      color: CustomTheme.instance.containerBgColor,
       child: ,
     );
   }
