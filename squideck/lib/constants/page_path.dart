c2e0d452e2669812b37f0c58b00d2a1c2014ab8c
ndjndj <teq.ndj@gmail.com>
2022-11-07 23:42:36 +0900

    f

--- squideck/lib/constants/page_path.dart
+++ squideck/lib/constants/page_path.dart
@@ -1,4 +1,4 @@
 export 'package:squideck/pages/blog.dart';
-export 'package:squideck/pages/welcome.dart';
+export 'package:squideck/pages/mainpage.dart';
 export 'package:squideck/main.dart';
 export 'package:squideck/pages/common_frame.dart';
