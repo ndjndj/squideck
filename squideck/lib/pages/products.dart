b848ba8b286e2f2821b93ebdc2fe069d6b40b701
ndjndj <teq.ndj@gmail.com>
2022-11-07 23:46:43 +0900

    f

---
+++ squideck/lib/pages/products.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class Products extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text('Products')
+      ),
+    );
+  }
+}
