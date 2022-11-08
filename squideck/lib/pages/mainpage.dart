62c16e0808e0ed9fc0d6eeb50ce3538dcad41678
ndjndj <teq.ndj@gmail.com>
2022-11-08 23:55:26 +0900

    f

--- squideck/lib/pages/mainpage.dart
+++ squideck/lib/pages/mainpage.dart
@@ -79,7 +79,6 @@ class Welcome extends StatelessWidget {
                                 ),
                               ),
                             ),
-
                           ],
                         ),
                       ),
