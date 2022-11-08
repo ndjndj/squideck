bd35c0fb267321dd86dbec306766160a02319309
ndjndj <teq.ndj@gmail.com>
2022-11-08 23:53:03 +0900

    f

--- squideck/lib/pages/mainpage.dart
+++ squideck/lib/pages/mainpage.dart
@@ -149,6 +149,280 @@ class Welcome extends StatelessWidget {
               ),
 
             ),
+            Container(
+              width: double.infinity,
+              height: 300,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Color(0xFF333333),
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 100),
+              child: Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                          Colors.white.withOpacity(0.4),
+                          BlendMode.dstATop,
+                        ),
+                        image: const NetworkImage(
+                          'assets/sampleimages/squid.png',
+                        ),
+                        fit: BoxFit.fitWidth
+                      ),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        padding: const EdgeInsets.only(top: 8.0, left: 16.0),
+                        child: Text(
+                          'About',
+                          style: TextStyle(
+                            color: CustomTheme.instance.letter,
+                            fontSize: 48
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 8.0, left: 24.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 120,
+                              child: Text(
+                                'Name',
+                                style: TextStyle(
+                                  color: CustomTheme.instance.letter,
+                                  fontSize: 24
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                child: Text(
+                                  ': Yuta Shinzawa',
+                                  style: TextStyle(
+                                    color: CustomTheme.instance.letter,
+                                    fontSize: 24
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 8.0, left: 24.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 120,
+                              child: Text(
+                                'Job',
+                                style: TextStyle(
+                                  color: CustomTheme.instance.letter,
+                                  fontSize: 24
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                child: Text(
+                                  ': Software developer',
+                                  style: TextStyle(
+                                    color: CustomTheme.instance.letter,
+                                    fontSize: 24
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 8.0, left: 24.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 120,
+                              child: Text(
+                                'Admirer',
+                                style: TextStyle(
+                                  color: CustomTheme.instance.letter,
+                                  fontSize: 24
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                child: Text(
+                                  ': Aimyon, Munenori kawasaki, Taro Okamoto etc.',
+                                  style: TextStyle(
+                                    color: CustomTheme.instance.letter,
+                                    fontSize: 24
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+            ),
+
+
+            Container(
+              width: double.infinity,
+              height: 300,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Color(0xFF333333),
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 100),
+              child: Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                      image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                          Colors.white.withOpacity(0.4),
+                          BlendMode.dstATop,
+                        ),
+                        image: const NetworkImage(
+                          'assets/sampleimages/squid.png',
+                        ),
+                        fit: BoxFit.fitWidth
+                      ),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        padding: const EdgeInsets.only(top: 8.0, left: 16.0),
+                        child: Text(
+                          'About',
+                          style: TextStyle(
+                            color: CustomTheme.instance.letter,
+                            fontSize: 48
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 8.0, left: 24.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 120,
+                              child: Text(
+                                'Name',
+                                style: TextStyle(
+                                  color: CustomTheme.instance.letter,
+                                  fontSize: 24
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                child: Text(
+                                  ': Yuta Shinzawa',
+                                  style: TextStyle(
+                                    color: CustomTheme.instance.letter,
+                                    fontSize: 24
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 8.0, left: 24.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 120,
+                              child: Text(
+                                'Job',
+                                style: TextStyle(
+                                  color: CustomTheme.instance.letter,
+                                  fontSize: 24
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                child: Text(
+                                  ': Software developer',
+                                  style: TextStyle(
+                                    color: CustomTheme.instance.letter,
+                                    fontSize: 24
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 8.0, left: 24.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 120,
+                              child: Text(
+                                'Admirer',
+                                style: TextStyle(
+                                  color: CustomTheme.instance.letter,
+                                  fontSize: 24
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                child: Text(
+                                  ': Aimyon, Munenori kawasaki, Taro Okamoto etc.',
+                                  style: TextStyle(
+                                    color: CustomTheme.instance.letter,
+                                    fontSize: 24
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+            ),
           ]
         ),
       ),
