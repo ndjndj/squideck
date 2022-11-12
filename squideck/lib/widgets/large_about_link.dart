103924c36436117076e9773012efc4ffa908c57e
ndjndj <teq.ndj@gmail.com>
2022-11-12 23:53:22 +0900

    f

---
+++ squideck/lib/widgets/large_about_link.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class LargeAboutLink extends StatelessWidget {
+  const LargeAboutLink({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text('About')
+      ),
+    );
+  }
+}
