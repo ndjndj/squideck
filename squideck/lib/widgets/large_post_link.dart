6944b351fcf02fb3fefba5dcff8fa2fa72beca37
ndjndj <teq.ndj@gmail.com>
2022-11-14 02:27:03 +0900

    f

--- squideck/lib/widgets/large_post_link.dart
+++ squideck/lib/widgets/large_post_link.dart
@@ -16,63 +16,66 @@ class LargePostLink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LargeLinkFrame(
-      margins: margins,
-      backgroundImage: 'assets/sampleimages/post.png',
-      frameChild: <Widget> [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              alignment: Alignment.bottomLeft,
-              padding: const EdgeInsets.only(top: 8.0, left: 16.0),
-              child: Text(
-                'Post',
-                style: TextStyle(
-                  color: CustomTheme.instance.letter,
-                  fontSize: 60
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, '/post'),
+      child: LargeLinkFrame(
+        margins: margins,
+        backgroundImage: 'assets/sampleimages/post.png',
+        frameChild: <Widget> [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                alignment: Alignment.bottomLeft,
+                padding: const EdgeInsets.only(top: 8.0, left: 16.0),
+                child: Text(
+                  'Post',
+                  style: TextStyle(
+                    color: CustomTheme.instance.letter,
+                    fontSize: 60
+                  ),
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 8.0, left: 24.0),
-              width: double.infinity,
-              child: Text(
-                'When my muscles say "No", I say "Yes".',
-                style: TextStyle(
-                  color: CustomTheme.instance.letter,
-                  fontSize: 36
+              Container(
+                padding: const EdgeInsets.only(top: 8.0, left: 24.0),
+                width: double.infinity,
+                child: Text(
+                  'When my muscles say "No", I say "Yes".',
+                  style: TextStyle(
+                    color: CustomTheme.instance.letter,
+                    fontSize: 36
+                  ),
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 16.0, left: 36.0, right: 36.0),
-              width: double.infinity,
-              child: Text(
-                'TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTe',
-                style: TextStyle(
-                  color: CustomTheme.instance.letter,
-                  fontSize: 24
+              Container(
+                padding: const EdgeInsets.only(top: 16.0, left: 36.0, right: 36.0),
+                width: double.infinity,
+                child: Text(
+                  'TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTe',
+                  style: TextStyle(
+                    color: CustomTheme.instance.letter,
+                    fontSize: 24
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 3,
                 ),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 3,
               ),
-            ),
-          ],
-        ),
-        Container(
-          alignment: Alignment.topRight,
-          padding: const EdgeInsets.only(top: 16.0, left: 36.0, right: 36.0),
-          width: double.infinity,
-          child: Text(
-            '2022/12/24 12:04',
-            style: TextStyle(
-              color: CustomTheme.instance.letter,
-              fontSize: 24
+            ],
+          ),
+          Container(
+            alignment: Alignment.topRight,
+            padding: const EdgeInsets.only(top: 16.0, left: 36.0, right: 36.0),
+            width: double.infinity,
+            child: Text(
+              '2022/12/24 12:04',
+              style: TextStyle(
+                color: CustomTheme.instance.letter,
+                fontSize: 24
+              ),
             ),
           ),
-        ),
-      ]
+        ]
+      )
     );
   }
 }
