817d20ad1ec4ceafea0ec3711e45fb6bddbafdde
ndjndj <teq.ndj@gmail.com>
2022-11-14 02:30:10 +0900

    f

--- squideck/lib/widgets/large_product_link.dart
+++ squideck/lib/widgets/large_product_link.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:squideck/constants/custom_theme.dart';
+import 'package:squideck/constants/route.dart';
 import 'package:squideck/widgets/large_link_frame.dart';
 
 
@@ -16,50 +17,53 @@ class LargeProductLink extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LargeLinkFrame(
-      margins: margins,
-      backgroundImage: 'assets/sampleimages/pickles.png',
-      frameChild: <Widget> [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              alignment: Alignment.bottomLeft,
-              padding: const EdgeInsets.only(top: 8.0, left: 16.0),
-              child: Text(
-                'Product',
-                style: TextStyle(
-                  color: CustomTheme.instance.letter,
-                  fontSize: 60
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, RouteGenerator.products),
+      child: LargeLinkFrame(
+        margins: margins,
+        backgroundImage: 'assets/sampleimages/pickles.png',
+        frameChild: <Widget> [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                alignment: Alignment.bottomLeft,
+                padding: const EdgeInsets.only(top: 8.0, left: 16.0),
+                child: Text(
+                  'Product',
+                  style: TextStyle(
+                    color: CustomTheme.instance.letter,
+                    fontSize: 60
+                  ),
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 8.0, left: 24.0),
-              width: double.infinity,
-              child: Text(
-                'We released simple Chrome extention "Pickles".',
-                style: TextStyle(
-                  color: CustomTheme.instance.letter,
-                  fontSize: 36
+              Container(
+                padding: const EdgeInsets.only(top: 8.0, left: 24.0),
+                width: double.infinity,
+                child: Text(
+                  'We released simple Chrome extention "Pickles".',
+                  style: TextStyle(
+                    color: CustomTheme.instance.letter,
+                    fontSize: 36
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-        Container(
-          alignment: Alignment.topRight,
-          padding: const EdgeInsets.only(top: 16.0, left: 36.0, right: 36.0),
-          width: double.infinity,
-          child: Text(
-            '2022/12/24 12:04',
-            style: TextStyle(
-              color: CustomTheme.instance.letter,
-              fontSize: 24
-            ),
+            ],
           ),
-        )
-      ]
+          Container(
+            alignment: Alignment.topRight,
+            padding: const EdgeInsets.only(top: 16.0, left: 36.0, right: 36.0),
+            width: double.infinity,
+            child: Text(
+              '2022/12/24 12:04',
+              style: TextStyle(
+                color: CustomTheme.instance.letter,
+                fontSize: 24
+              ),
+            ),
+          )
+        ]
+      )
     );
   }
 }
