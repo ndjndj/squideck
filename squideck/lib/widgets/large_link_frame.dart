3df3f6d501a475ffcf9f3109f8008e91ab9d03dd
ndjndj <teq.ndj@gmail.com>
2022-11-14 02:07:55 +0900

    f

--- squideck/lib/widgets/large_link_frame.dart
+++ squideck/lib/widgets/large_link_frame.dart
@@ -25,7 +25,7 @@ class LargeLinkFrame extends StatelessWidget {
         borderRadius: BorderRadius.all(Radius.circular(10)),
         color: Color(0xFF333333),
       ),
-      margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20, bottom: 100),
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20, bottom: 20),
       child: Stack(
         children: [
           Container(
