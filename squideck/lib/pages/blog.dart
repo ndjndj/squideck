1393eeb824837c9ad769e7c9989a19a1f34fdcf8
ndjndj <teq.ndj@gmail.com>
2022-11-21 00:03:11 +0900

    f

--- squideck/lib/pages/blog.dart
+++ squideck/lib/pages/blog.dart
@@ -8,28 +8,30 @@ class Blog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-      child: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 8.0),
-            width: double.infinity,
-            child: Text(
-              'Blog posts',
-              style: TextStyle(
-                color: CustomTheme.instance.dark,
-                fontSize: 60,
-              )
+    return SingleChildScrollView(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 8.0),
+              width: double.infinity,
+              child: Text(
+                'Blog posts',
+                style: TextStyle(
+                  color: CustomTheme.instance.dark,
+                  fontSize: 60,
+                )
+              ),
             ),
-          ),
-          PostTop(),
-          LargePostLink(),
-          LargePostLink(),
-          LargePostLink(),
-          LargePostLink(),
-        ],
-      )
+            PostTop(),
+            LargePostLink(),
+            LargePostLink(),
+            LargePostLink(),
+            LargePostLink(),
+          ],
+        )
+      ),
     );
   }
 }
